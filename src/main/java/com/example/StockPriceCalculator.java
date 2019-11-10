e5c666a9af4861f97df3e00f6aabe0954e2e6e50
Abul Basar <abulbasar@gmail.com>
2019-11-11 00:32:56 +0530

    update

--- src/main/java/com/example/StockPriceCalculator.java
+++ src/main/java/com/example/StockPriceCalculator.java
@@ -3,6 +3,7 @@ package com.example;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
@@ -13,8 +14,19 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 /*
-Sample data file: https://github.com/abulbasar/data/blob/master/stocks.small.csv
+
 Problem statement: find average volume of trading per stock symbol.
+
+Build jar
+$ mvn package
+
+$ sudo -u hdfs hadoop fs -mkdir -p /data
+$ sudo -u hdfs hadoop fs -chmod 777 /data
+$ wget https://raw.githubusercontent.com/abulbasar/data/master/stocks.small.csv
+$ sudo -u hdfs hadoop fs -mkdir /data/stocks
+$ hadoop fs -put stocks.small.csv /data/stocks
+$ hadoop jar HadoopMRExamples-1.0-SNAPSHOT.jar com.example.WordCount /data/stocks.small.csv /data/output
+
 
 */
 public class StockPriceCalculator {
@@ -57,15 +69,27 @@ public class StockPriceCalculator {
   }
 
   public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "Stock price calculator");
-    job.setJarByClass(StockPriceCalculator.class);
-    job.setMapperClass(TokenizerMapper.class);
-    job.setReducerClass(AvgReducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(DoubleWritable.class);
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
+        final Path inputPath = new Path(args[0]);
+        final Path outputPath = new Path(args[1]);
+
+
+        /*
+        * Delete the output directory if it exists.
+        */
+        Configuration conf = new Configuration();
+        final FileSystem fileSystem = inputPath.getFileSystem(conf);
+        if(fileSystem.exists(outputPath)){
+          fileSystem.delete(outputPath, true);
+        }
+
+        Job job = Job.getInstance(conf, "Stock price calculator");
+        job.setJarByClass(StockPriceCalculator.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setReducerClass(AvgReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
+        FileInputFormat.addInputPath(job, inputPath);
+        FileOutputFormat.setOutputPath(job, outputPath);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
